5068eeb15ce4dd0334dcb745e4b657f2d9e05246
solarcalf <112930470+solarcalf@users.noreply.github.com>
2023-11-15 21:46:56 +0300

    Numeric method implementation
    Have to be tested

--- ThermalConduction.h
+++ ThermalConduction.h
@@ -4,8 +4,10 @@
 #include <vector>
 #include "TMA.h"
 
+#include <iostream>
+
 class thermal {
-private:
+public:
 
 	using FP = double;
 	using VecFP = std::vector<FP>;
@@ -14,54 +16,99 @@ private:
 	VecFP x_grid;
 	VecFP v_vals;
 
-	static const inline FP integrate(FP a, FP h, std::function<FP(FP)> f) {
-		return f(a + h) * h;
-	}
-
 public:
-	thermal (FP a, FP b, size_t N, FPair k, FPair q, FPair f, FP mu1, FP mu2, FP xi) {
+	thermal (FP x0, FP xn, size_t N, FPair k, FPair q, FPair f, FP mu1, FP mu2, FP xi) {
 		
-		FP h = (b - a) / N;
-		VecFP x_grid{a};
-		VecFP v_vals;
+		VecFP a(N);
+		VecFP d(N);
+		VecFP phi(N);
+
+		FP h = (xn - x0) / N;
+		VecFP x(N + 1);
+		x[0] = x0;
+		x[1] = x0 + h;
+
+		size_t i = 1;
+
+		while (xi >= x[i] + h / 2) {
+			a[i-1] = k.first(x[i] - h / 2);
+			d[i-1] = q.first(x[i]);
+			phi[i-1] = f.first(x[i]);
+
+			x[i + 1] = x[i] + h;
+			i += 1;
+		}
+
+		if (x[i] - h / 2 <= xi && xi <= x[i] + h / 2) {
+			if (xi < x[i])
+				a[i-1] = h / ((xi - x[i - 1]) / k.first((x[i - 1] + xi) / 2) + (x[i] - xi) / k.second((xi + x[i]) / 2));
+			else
+				a[i-1] = k.first(x[i] - h / 2);
+
+			FP xl = x[i] - h / 2;
+			FP xr = x[i] + h / 2;
+			d[i-1] = (q.first((xl + xi) / 2) * (xi - xl) + q.second((xi + xr) / 2) * (xr - xi)) / h;
+			phi[i-1] = (f.first((xl + xi) / 2) * (xi - xl) + f.second((xi + xr) / 2) * (xr - xi)) / h;
+			x[i + 1] = x[i] + h;
+			i += 1;
+		}
+
+		if (x[i - 1] < xi && xi < x[i]) {
+			a[i-1] = h / ((xi - x[i - 1]) / k.first((x[i - 1] + xi) / 2) + (x[i] - xi) / k.second((xi + x[i]) / 2));
+
+			d[i-1] = q.second(x[i]);
+			phi[i-1] = f.second(x[i]);
+
+			x[i + 1] = x[i] + h;
+			i += 1;
+		}
+
+		while (i != N) {
+			a[i-1] = k.second(x[i] - h / 2);
+			d[i-1] = q.second(x[i]);
+			phi[i-1] = f.second(x[i]);
+
+			x[i + 1] = x[i] + h;
+			i += 1;
+		}
+
+		a[i-1] = k.second(x[i] - h / 2);
+		d[i-1] = q.second(x[i]);
+		phi[i-1] = f.second(x[i]);
+
+		phi[0] = mu1;
+		phi[N-1] = mu2;
 
 		VecFP A(N - 1);
 		VecFP B(N - 1);
-		VecFP C(N - 1);
-		VecFP phi(N);
+		VecFP C(N);
 
-		FP a = integrate(x_grid.back(), h, [k](FP x) { return 1 / k.first(x); });
-		size_t i = 1;
-
-		A[0] = 1 / (a + h);
-
-		while (x_grid.back() + h < xi) {
-			FP a = integrate(x_grid.back(), h, [k](FP x) { return 1 / k.first(x); });
-
-			A[i] = 1 / (a * h);
-			B[i - 1] = A[i];
-			C[i] += A[i];
-			C[i - 1] += A[i++];
+		A[N - 2] = 0;
+		B[0] = 0;
+		C[0] = 1;
+		C[N - 1] = 1;
+			
+		for (i = 1; i < N - 1; i++) {
+			A[i - 1] = a[i - 1] / pow(h, 2);
+			B[i] = a[i + 1] / pow(h, 2);
+			C[i] = -1 * ((a[i] + a[i + 1]) / pow(h, 2) + d[i]);
 		}
 		
-		FP a = integrate(x_grid.back(), xi - x_grid.back(), [k](FP x) { return 1 / k.first(x); }) +
-			integrate(xi, x_grid.back() + h - xi, [k](FP x) { return 1 / k.second(x); });
+		//=================================DEBUG=====================================//
+		/*
+		std::cout << 'i' << '\t' << 'a' << "\t\t" << 'd' << "\t\t" << "phi\n\n";
+		for (i = 0; i < N - 1; i++)
+			std::cout << i << '\t' << a[i] << "\t\t" << d[i] << "\t\t" << phi[i] << '\n';
 
-		A[i] = 1 / (a * h);
-		B[i - 1] = A[i];
-		C[i] += A[i];
-		C[i - 1] += A[i++];
-		
-		while (x_grid.back() < b) {
-			FP a = integrate(x_grid.back(), h, [k](FP x) { return 1 / k.second(x); });
+		std::cout << "\n\n\n";
 
-			A[i] = 1 / (a * h);
-			B[i - 1] = A[i];
-			C[i] += A[i];
-			C[i - 1] += A[i++];
-		}
+		std::cout << 'i' << '\t' << 'A' << "\t\t" << 'B' << "\t\t" << "C\n\n";
+		for (i = 0; i < N - 1; i++)
+			std::cout << i << '\t' << A[i] << "\t\t" << B[i] << "\t\t" << C[i] << '\n';
+		std::cout << C[N - 1];
+		*/
 
-
+		x_grid = std::move(a);
+		v_vals = TMA(A, C, B, phi);
 	}
-
 };
